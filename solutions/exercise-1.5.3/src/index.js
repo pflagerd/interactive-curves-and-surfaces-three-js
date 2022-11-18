1776d3c998adcb9906c093eab765e10ce03a5f8b
Daniel P Pflager <daniel.pflager@gmail.com>
2022-11-17 20:46:44 -0800

    progress

--- solutions/exercise-1.5.3/src/index.js
+++ solutions/exercise-1.5.3/src/index.js
@@ -6,17 +6,18 @@ function main() {
     document.body.style.overflow = "hidden";
 
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xFFFFFF)
 
     // CAMERA
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const camera = new THREE.OrthographicCamera( -10, 10, 10, -10, 1, 1000 );
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    camera.position.z = 15;
+    camera.position.z = 20;
 
-    const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+    const material = new THREE.LineBasicMaterial({color: 0x000000});
 
     const points = [];
     points.push( new THREE.Vector3( -10, 0, 0 ) );
